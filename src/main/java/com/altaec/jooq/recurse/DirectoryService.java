c521b4135dc8f63cb15663030f9efeba20a9ecc9
alekseev <antonalekseevv@gmail.com>
2023-01-24 15:08:19 +0700

    Обёртка sql -> jooq java

--- src/main/java/com/altaec/jooq/recurse/DirectoryService.java
+++ src/main/java/com/altaec/jooq/recurse/DirectoryService.java
@@ -1,25 +1,25 @@
 package com.altaec.jooq.recurse;
 
 
-import org.jooq.CommonTableExpression;
-import org.jooq.DSLContext;
-
+import org.jooq.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
-import static com.altaec.recurse.jooq.sample.model.Tables.*;
+import static com.altaec.recurse.jooq.sample.model.Tables.DIRECTORY;
 import static org.jooq.impl.DSL.*;
-import static org.jooq.impl.SQLDataType.*;
-
+import static org.jooq.impl.SQLDataType.INTEGER;
 
 @Service
 public class DirectoryService {
     @Autowired
     DSLContext context;
 
-    public void getAllRecurseDirectory() {
+    /**
+     * Метод возвращает все записи в виде иархического дерева, из таблицы DIRECTORY
+     */
+    public SelectWhereStep<?> getAllRecurseDirectory() {
+        var array = array(DIRECTORY.ID);
+        var dataTypeId = DIRECTORY.ID.getDataType();
         CommonTableExpression<?> cte = name("t").fields(
                 "id",
                 "name",
@@ -28,28 +28,45 @@ public class DirectoryService {
                 select(
                         DIRECTORY.ID,
                         DIRECTORY.LABEL,
-                        DIRECTORY.LABEL)
+                        array)
                         .from(DIRECTORY)
                         .where(DIRECTORY.PARENT_ID.isNull())
                         .unionAll(
                                 select(
                                         DIRECTORY.ID,
                                         DIRECTORY.LABEL,
-                                        field(name("t", "path"), VARCHAR)
-                                                .concat("\\")
-                                                .concat(DIRECTORY.LABEL))
+//                                        arrayAppend(array, field(name("t", "path"),INTEGER)))
+                                        function("array_append", array.getDataType(), field(name("t", "path")), DIRECTORY.ID))
+//                                        arrayAppend(array, field(name("t", "path"),INTEGER)))
                                         .from(table(name("t")))
                                         .join(DIRECTORY)
-                                        .on(field(name("t", "id"), INTEGER)
+                                        .on(field(name("t", "id"), dataTypeId)
                                                 .eq(DIRECTORY.PARENT_ID)))
         );
 
-        System.out.println(
-                context.withRecursive(cte)
-                        .selectFrom(cte)
-                        .fetch()
-        );
+        return context.withRecursive(cte)
+                .selectFrom(cte);
 
+
+
+    }
+
+    /**
+     * Получить иархически построенные данные таблицы, с условием что выбран был элемент searchField, с лимитом;
+     * Родительские элементы обязаны возвращаться всегда
+     */
+    public void getResultWithLimitData() {
+        var searchField = "Sub Menu 2";
+        System.out.println(getAllRecurseDirectory().where(exists(
+                select(inline(1)).from(
+                        select()
+                                .from(table(quotedName("t")))
+                                .where(field(name("t", "name")).eq(inline(searchField)))
+                                .limit(inline(1))
+                                .asTable("with_limit_table")
+                        //todo разобраться с ANY
+                ).where(field(name("t", "id")).eq(function("any", INTEGER, field(name("with_limit_table", "path")))))
+        )).fetch());
     }
 
 
--- src/test/java/com/altaec/jooq/recurse/JooqDemoRecurseApplicationTests.java
+++ src/test/java/com/altaec/jooq/recurse/JooqDemoRecurseApplicationTests.java
@@ -1,13 +1,18 @@
 package com.altaec.jooq.recurse;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JooqDemoRecurseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DirectoryService directoryService;
+
+    @Test
+    void contextLoads() {
+        directoryService.getResultWithLimitData();
+    }
 
 }
